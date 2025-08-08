c42d211b4188df348c5598b409111502520304fd
Lachlan McCrae <lachlan@mccrae.nz>
2025-08-08 22:01:03 +0700

    fix plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = () => ({
+  documentation: {
+    enabled: true,
+    config: {
+      path: "./src/extensions/documentation/documentation",
+      info: {
+        title: "My API Docs",
+        version: "1.0.0",
+      },
+    },
+  },
+});
